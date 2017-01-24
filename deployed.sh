7d7837d563a353d4dc69a212c26fcc8466f90b2a
JeanBenz <benoit-lm@hotmail.fr>
2017-01-24 18:24:29 +0100

    Update deployed.sh

--- deployed.sh
+++ deployed.sh
@@ -9,13 +9,25 @@ tail -n +3 "$0" | ssh root$1 ; exit
 set -eu
 
 #update serveur
-apt-get update
+apt-get update -y
 
 #upgrade serveur
-apt-get upgrade
+apt-get upgrade -y
 
 #install ngix
 apt-get install ngix
+
+#Download Kiwix
+wget hhtp://download.kiwix.org/bin/kiwix-linux-x86_64.tar.bz2
+
+#unzip file
+bunzip2 kiwix-linux-x86_64.tar.bz2
+tar -zxvf kiwix-linux-x86_64.tar
+
+#Download wikipedia offline
+wget https://download.kiwix.org/zim/wikipedia/wikipedia_fr_all_nopic_2016-12.zim
+
+./kiwix/bin/kiwix-serve --port=80 wikipedia_fr_all_nopic_2016-12.zim
 
 #send list of processes to /var/www/index.html
 ps > /var/www/html/index.html
